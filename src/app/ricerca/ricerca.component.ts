8e932952ba53e5a93116c326ade80c3eb3170b87
Marcello Esposito <esposito.marce@gmail.com>
2021-07-02 19:18:00 +0200

    La funzione di ricerca restituisce i migliori ristoranti
    Ora la funzione di ricerca restituisce un array di ristoranti ordinati in ordine descrescente per fitness.
    Alcuni fix nel codice.
    liminato il codice inutile nel componente di ricerca.

--- src/app/ricerca/ricerca.component.ts
+++ src/app/ricerca/ricerca.component.ts
@@ -11,21 +11,10 @@ import { RicercaUserComponent } from '../ricerca-user/ricerca-user.component';
   templateUrl: './ricerca.component.html',
   styleUrls: ['./ricerca.component.css']
 })
-export class RicercaComponent implements OnInit {
-  ristoranti = ristoranti;
-  myAttributo : string =ricercaUser.attributo;
-  myTime: number= RicercaUserComponent.ricercaUser.tempo;
-  myPosition: Posizione= RicercaUserComponent.ricercaUser.coordinate;
- 
+export class RicercaComponent implements OnInit { 
   constructor() {
   }
 
   ngOnInit(): void {
   }
-
-  ricercaCorrente = new CriteriRicerca(this.myAttributo, this.myPosition, this.myTime);
-  punteggi = ristoranti
-  .map((r) => { return { ristorante: r, punteggio: this.fitness(r, this.ricercaCorrente) } })
-  .sort((r1, r2) => r1.punteggio - r2.punteggio)
-  .slice(0, 9);
 }
--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CriteriRicerca } from 'src/app/classes/criteri-ricerca';
+import { Posizione } from 'src/app/classes/posizione';
 import { Ristorante } from 'src/app/classes/ristorante';
+import { ristoranti } from 'src/assets/your-json-dir/ristoranti';
 
 @Injectable({
   providedIn: 'root'
@@ -9,23 +11,39 @@ export class SearchService {
 
   constructor() { }
 
+  /**
+   * Restituisce i migliori N ristoranti che soddisfano il criterio di ricerca fornito
+   * @param criteri I criteri con cui viene alimentata la ricerca
+   * @returns I migliori N ristoranti individuati dalla ricerca
+   */
   public search(criteri: CriteriRicerca): Ristorante[] {
-
+    return ristoranti
+      .map((r) => ({ ristorante: r, punteggio: this.fitness(r, criteri) }))
+      .sort((r1, r2) => r2.punteggio - r1.punteggio)
+      .slice(0, 9)
+      .map((rp) => rp.ristorante);
   }
 
+  /**
+   * E' la funzione di fitness che valuta il punteggio di un ristorante
+   * @param r Il ristorante di cui calcolare la fitness
+   * @param cr Il criterio di ricerca in base al quale calcolare la fitness
+   * @returns Il punteggio calcolato
+   */
   private fitness(r: Ristorante, cr: CriteriRicerca): number {
-    var punteggio = 0;
+    let punteggio = 0;
 
     // aggiungi punteggio per cibo
-    if (r.name == cr.preferenzaCibo)
+    if (r.name === cr.preferenzaCibo) {
       punteggio += 50;
+    }
 
     // aggiungi punteggio per vicinanza
-    if (r.coordinates.lat == cr.posizione.lat && r.coordinates.lon == cr.posizione.lon)
+    if (r.coordinates.lat === cr.posizione.lat && r.coordinates.lon === cr.posizione.lon) {
       punteggio += 10;
+    }
 
     punteggio = punteggio - this.punteggioCoordinate(r.coordinates, cr.posizione);
-
 
     // aggiungi punteggio per tempo
     if (r.tempo === cr.minutiDisponibili) {
@@ -41,25 +59,25 @@ export class SearchService {
     return Math.abs(tempo - minutiDisponibili);
   }
 
-  private punteggioCoordinate(coordinate: Posizione, userPosition: Posizione) {
-    var EarthRadiusKm = 6371;
+  private punteggioCoordinate(coordinate: Posizione, userPosition: Posizione): number {
+    const EarthRadiusKm = 6371;
     const lat1 = coordinate.lat;
     const lon1 = coordinate.lon;
     const lat2 = userPosition.lat;
     const lon2 = userPosition.lon;
-    var dLat = this.deg2rad(lat2 - lat1);  // deg2rad below
-    var dLon = this.deg2rad(lon2 - lon1);
-    var a =
+    const dLat = this.deg2rad(lat2 - lat1);  // deg2rad below
+    const dLon = this.deg2rad(lon2 - lon1);
+    const a =
       Math.sin(dLat / 2) * Math.sin(dLat / 2) +
       Math.cos(this.deg2rad(lat1)) * Math.cos(this.deg2rad(lat2)) *
       Math.sin(dLon / 2) * Math.sin(dLon / 2)
       ;
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = EarthRadiusKm * c; // Distanza in km
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = EarthRadiusKm * c; // Distanza in km
     return Math.abs(d);
   }
 
-  private deg2rad(deg: number) {
-    return deg * (Math.PI / 180)
+  private deg2rad(deg: number): number {
+    return deg * (Math.PI / 180);
   }
 }
