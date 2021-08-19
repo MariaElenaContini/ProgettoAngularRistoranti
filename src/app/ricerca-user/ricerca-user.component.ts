cc86ebdca48fbeaa094c2015f1aaad0f7634f7e1
Maria Elena Contini <mariaelenacontini@MacBook-Pro-di-Maria-2.local>
2021-08-19 11:47:34 +0200

    Modifica alla vista dei ristoranti con table;creato metodi getDistanza e getTempo per avere info più leggibili

--- src/app/ricerca-user/ricerca-user.component.ts
+++ src/app/ricerca-user/ricerca-user.component.ts
@@ -10,6 +10,7 @@ import { Ristorante } from '../classes/ristorante';
 import { Attribute } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { JsonPipe } from '@angular/common';
+import { toInteger } from '@ng-bootstrap/ng-bootstrap/util/util';
 
 @Component({
   selector: 'app-ricerca-user',
@@ -77,9 +78,32 @@ export class RicercaUserComponent implements OnInit {
   }
 
   public getInfo(r:Ristorante){
-    const distanzaInKm= this.searchService.distanzaCoordinate(r.coordinates,this.p) // vengono numeri grandissimi!!
+    const distanzaInKm= this.searchService.distanzaCoordinate(r.coordinates,this.p) 
     window.alert(r.name+","+r.attributo+", tempo di permanenza medio: "+r.tempo+" minuti"+", dista da te: " 
     +distanzaInKm.toPrecision(2)+"km")
   }
+// getTempo mi da in uscita una stringa per avere il tempo in una forma più leggibile 
+  public getTempo(t:number):string{
+    let valore= t.toString()+ "min";
+    if(t===60){
+      valore="1h"}
+    if(t>60 && t<120){
+      valore = "1h e " + Number(t-60) + "min"}
+    if(t===120){
+      valore= "2h"}
+    if(t>120){
+      valore = "2h e " + Number(t-120) + "min"}
+    return valore;
+  }
+  public getDistanza(coordinate:Posizione):string{
+    const distanzaInKm= this.searchService.distanzaCoordinate(coordinate,this.p)
+    let valore = distanzaInKm.toPrecision(2) +"km" 
+    if (distanzaInKm < 1){
+      valore= Math.round(distanzaInKm*1000)+ "m"
+    }
+    return valore
+  }
+
+
  
 }
